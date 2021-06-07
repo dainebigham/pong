e9aeabd757955ffe3825bf2a9cd75490d72c37a1
= <daine.bigham@gmail.com>
2021-06-07 17:21:41 +1200

    create the paddle class which includes 2 paddles as well as methods to control them. Disabled screen tracer and update the screen in a loop instead

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-from turtle import Screen
+from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor('black')
 screen.title('PyPong')
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+screen.listen()
+screen.onkey(r_paddle.up, 'Up')
+screen.onkey(r_paddle.down, 'Down')
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
 
 
 screen.exitonclick()
